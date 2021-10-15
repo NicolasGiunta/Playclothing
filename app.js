49b9eb3a42cc2951550137689f545c69c9eec6e5
dianapolanco <dianacarolina.polanco@gmail.com>
2021-10-15 14:44:56 -0500

    ajustes en los archivos html y css del index

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "./views/index.html"))
 });
 
+app.get('/producto', (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
+}
+);
+
+app.get('/cuenta', (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/sigIn-signOut-Form.html"))
+}
+);
+
 app.listen(PUERTO, ()=>{console.log("Andando sin problema")});
 
 
