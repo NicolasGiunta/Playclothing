5be9e7d07e35a83fb96caf0d289f28a2ece63a48
galantejulian <galante.julian@gmail.com>
2021-10-30 11:32:17 -0300

    modificando rutas

--- app.js
+++ app.js
@@ -1,42 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require ('path');
+const path = require("path");
 const PUERTO = 3000;
-const productRoutes = require('./routes/productsRoutes')
-const mainRoutes = require('./routes/mainRoutes')
+const productRoutes = require("./routes/productsRoutes");
+const mainRoutes = require("./routes/mainRoutes");
+const usersRoutes = require("./routes/usersRoutes");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use('/', mainRoutes)
+app.use("/", mainRoutes);
 
-app.use('/producto', productRoutes)
+app.use("/producto", productRoutes);
 
 //por cambiar
 
-app.get('/cuenta', (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/sigIn-signOut-Form.html"))
-}
-);
+app.use("/cuenta", usersRoutes);
 
+app.get("/carrito", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/carrito_compras.html"));
+});
 
-app.get('/carrito', (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/carrito_compras.html"))
-}
-);
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register_form.html"));
+});
 
-app.get('/registro', (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/register_form.html"))
-}
-);
+app.post("/micuenta", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
 
-app.post('/micuenta', (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/index.html"))
-}
-);
-
-
-app.listen(PUERTO, ()=>{console.log("Andando sin problema")});
-
-
+app.listen(PUERTO, () => {
+  console.log("Andando sin problema");
+});
---
+++ controllers/usersController.js
@@ -0,0 +1,7 @@
+const usersController = {
+  login: function (req, res) {
+    res.render("sigIn-signOut-Form");
+  },
+};
+
+module.exports = usersController;
