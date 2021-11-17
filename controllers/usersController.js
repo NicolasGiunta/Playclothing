c4b8200ec7860335e2121e55fcb21a1b35cd5433
galantejulian <galante.julian@gmail.com>
2021-11-16 23:06:37 -0300

    agregamos users controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,25 @@
+const path = require('path');
+const fs = require('fs');
+const UsersFilePath = path.join(__dirname, '../data/users.json');
+let usuarios;
+const listaUsuarios = fs.readFileSync(UsersFilePath, 'utf-8');  
+if (listaUsuarios == ""){
+  usuarios=[];
+}else{
+  usuarios=JSON.parse(listaUsuarios)
+}
+
+const nuevoId = () => {
+  let ultimo = 0;
+  usuarios.forEach(usuario => {
+    if (usuario.id > ultimo) {
+      ultimo = usuario.id;
+    }
+  });
+  return ultimo + 1;
+}
+
+
 const usersController = {
   login: function (req, res) {
     res.render("sigIn-signOut-Form");
@@ -14,16 +36,23 @@ const usersController = {
   //para guardar la infor de un usuario nuevo y que lo eenvie al home
   create:  function (req, res) {
     let usuario = {
-     email:req.body.email,
-     password: req.body.password
+    id: nuevoId(),
+    nombre:req.body.nombre,
+    apellido:req.body.apellido,
+    categoria:req.body.categoria,
+    email:req.body.email,
+    sexo:req.body.sexo,
+    password: req.body.password
     }
-  
-      //guardarla falta ver bases de datos
-  
-    res.redirect("/")
-  }
+usuarios.push(usuario)
+usuariosJSON = JSON.stringify(usuarios);
+fs.writeFileSync(UsersFilePath, usuariosJSON)  
+res.redirect('/')
+}
 
-  
-};
+
+}
+
+
 
 module.exports = usersController;
