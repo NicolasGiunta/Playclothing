b7663634ee606c93e86d44fcd803902ec337eb7f
unknown <nicolas.giunta1@gmail.com>
2022-01-28 19:28:51 -0300

    register js casi terminado

--- public/js/register.js
+++ public/js/register.js
@@ -7,14 +7,15 @@ let nombre = document.querySelector (".nombre");
 let apellido = document.querySelector(".apellido");
 let email = document.querySelector("#reg_email")
 let password = document.querySelector("#password");
-let confirmpassword = document.querySelector(".confirmPassword");
-let sexo = document.querySelector(".sexo");
+let confirmPassword = document.querySelector("#confirmPassword");
+
 
 let erroresNombre = document.querySelector ("div .erroresNombre");
 let erroresApellido = document.querySelector("div .erroresApellido");
 let erroresEmail = document.querySelector ("div .erroresEmail");
 let erroresContraseña = document.querySelector("div .erroresContraseña");
-
+let erroresConfirmarContraseña = document.querySelector("div .erroresConfirmarContraseña");
+let erroresSexo = document.querySelector("div .erroresSexo");
 
 form.addEventListener("submit", function(e){ 
     e.preventDefault()
@@ -22,6 +23,8 @@ let errorsName = [];
 let errorsLastName = [];
 let errorsEmail = [];
 let errorsPassword = [];
+let errorsConfirmPassword = [];
+let erroresSex = [];
 
 
 
@@ -95,19 +98,23 @@ if (errorsEmail.length > 0  ){
 
     //validar  contraseña
 
-    let validarContraseña = /^(?=(?:.\d){2})(?=(?:.[A-Z]){2})(?=(?:.*[a-z]){2})\S{8,}$/;
-    let validacionContraseña = validarContraseña.test(password.value)
+    // let validarContraseña = new RegExp("^(?=.[a-z])(?=.[A-Z])(?=.[0-9])(?=.[!@#$%^&*])");
+    //let validacionContraseña = validarContraseña.test(password.value);
+
+    //console.log(validacionContraseña);
 
 if(password.value== ""){
 errorsPassword.push("Escribe tu contraseña")
 
+}else if(password.value.length < 8){
+    errorsPassword.push('La contraseña debe tener como minimo 8')
 }
 
 
-else if (validacionContraseña == false) {
-    errorsPassword.push('La contraseña debe tener como mínimo 8 caractéres, incluídos 1 letra mayúscula, 1 minúscula y 1 caracter especial')
+// else if (validacionContraseña == false) {
+//     errorsPassword.push('La contraseña debe tener como mínimo 8 caractéres, incluídos 1 letra mayúscula, 1 minúscula y 1 caracter especial')
     
-}
+// }
  
 if (errorsPassword.length > 0  ){
     
@@ -119,6 +126,47 @@ if (errorsPassword.length > 0  ){
             
        
     }
+
+    let contra = password.value
+    let confirmcontra =confirmPassword.value
+     
+   if(contra != confirmcontra){
+        errorsConfirmPassword.push('Las contraseñas no coinciden')
+    }
+    
+    
+    if (errorsConfirmPassword.length > 0  ){
+    
+        errorsConfirmPassword.forEach(error => {
+            erroresConfirmarContraseña.innerHTML = "<p> <small>" + error + "</small> </p>" 
+        }); 
+    
+            
+       
+    }
+
+    
+    if(!document.querySelector('input[name="sexo"]:checked')) {
+        erroresSex.push('Elige un sexo');
+        
+        }
+        if (erroresSex.length > 0){
+        erroresSex.forEach(error =>{
+            erroresSexo.innerHTML ="<p> <small>" + error + "</small> </p>"
+    
+        })
+    }
+
+
+    
+        // var fileInput = document.getElementById('file');
+        // var filePath = fileInput.value;
+        // var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
+        // if(!allowedExtensions.exec(filePath)){
+        //     alert('Please upload file having extensions .jpeg/.jpg/.png/.gif only.');
+        //     fileInput.value = '';
+        //     return false;
+        // }
 
 })
 
